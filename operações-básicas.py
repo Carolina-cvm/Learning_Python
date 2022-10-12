38415a6f0d46edc87ccd9d2c792e3adc0f256cf6
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-12 12:03:02 -0300

    Update operações-básicas.py

--- operações-básicas.py
+++ operações-básicas.py
@@ -1,30 +1,38 @@
 def soma():
-  x = num1
-  y = num2
   contasoma = x + y
   print("Você escolheu soma:", x, "+", y, "=", contasoma)
-  
+
+
 def subtracao():
-  x = num1
-  y = num2
   contasubtracao = x - y
   print("Você escolheu subtração:", x, "-", y, "=", contasubtracao)
 
+
 def divisao():
-  x = num1
-  y = num2
   contadivisao = x / y
   print("Você escolheu divisão:", x, "/", y, "=", contadivisao)
 
+
 def multiplicacao():
-  x = num1
-  y = num2
   contamultiplicacao = x * y
   print("Você escolheu multiplicação:", x, "*", y, "=", contamultiplicacao)
-  
+
+
 conta = input("Qual operação básica? Ex: soma, subtração, multiplicação ou divisão ")
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+
+try:
+  x = int(num1)
+  y = int(num2)
+except ValueError:
+  while num1.isdigit() == False or num2.isdigit() == False:
+    print("Digite apenas números!")
+    num1 = input("Digite o primeiro número novamente: ")
+    num2 = input("Digite o segundo número novamente: ")
+    x = int(num1)
+    y = int(num2)
+  
 
 if conta == "soma" or conta == "Soma":
   soma()
@@ -36,5 +44,4 @@ elif conta == "multiplicacao" or conta == "Multiplicação" or conta == "multipl
   multiplicacao()
 else:
   print("Digite o tipo de conta novamente ")
-
 
