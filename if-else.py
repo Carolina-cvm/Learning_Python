1763949e7a73ac7ece041d3fc30c24613156ba17
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-13 10:45:22 -0300

    Update and rename if-elif-else.py to if-else.py

--- if-else.py
+++ if-else.py
@@ -1,33 +1,28 @@
-print("Digite as informações sobre a criança:")
-
-nomecrianca = input("Qual o nome completo da criança?")
-nomeresponsavel = input("Qual o nome completo do responsável pela criança?")
-idaderesponsavel = int(input("Qual a idade do responsável pela criança?"))
-
 sibilou = input("A criança já sibilou uma vez na vida?")
-if sibilou == "sim":
+
+if sibilou == "sim" or sibilou == "Sim" or sibilou == "s":
   idade = input("A idade da criança é maior ou igual a 3 anos?")
-  if idade == "sim":
+  if idade == "sim" or idade == "Sim" or idade == "s":
     print("Provavelmente NÃO é asma induzida por vírus. Provável asma atópica, encaminhar ao especialista para iniciar tratamento preventivo.")
   else:
     criterio = input("A criança apresenta ao menos um dos critérios maiores?\n")
-    if criterio == "sim":
+    if criterio == "sim" or criterio == "Sim" or criterio == "s":
       criteriomenos3x = input("Sibilou menos que 3x ao dia? (Dois episódios no total).\n")
-      if criteriomenos3x == "sim":
+      if criteriomenos3x == "sim" or criteriomenos3x == "Sim" or criteriomenos3x == "s":
         print("Provavelmente NÃO é asma induzida por vírus. Provável asma atópica de início recente, encaminhar ao especialista para verificar necessidade de tratamento preventivo no futuro.")
       else:
         print("Provavelmente NÃO é asma induzida por vírus. Provável asma atópica estabelecida, encaminhar ao especialista para acompanhar e iniciar tratamento preventivo.")
     else:
       naoapresentacriterio = input("A criança não apresenta critérios menores?\n")
-      if naoapresentacriterio == "sim":
+      if naoapresentacriterio == "sim" or naoapresentacriterio == "Sim" or naoapresentacriterio == "s":
         naoapresentacriteriovezes = input("Sibilou menos que 3x ao dia? (Dois episódios no total).\n")
-        if naoapresentacriteriovezes == "sim":
+        if naoapresentacriteriovezes == "sim" or naoapresentacriteriovezes == "Sim" or naoapresentacriteriovezes == "s":
           print("Provavelmente É asma induzida por vírus. Acompanhá-la periodicamente, não necessita iniciar o tratamento preventivo.")
         else:
           print("Provavelmente É asma induzida por vírus. Acompanhá-la periodicamente, com eventos frequentes a decisão é individual pelo início do tratamento preventivo.")
       else:
           apresentaumcriterio = input("A criança apresenta ao menos um dos critérios menores?\n")
-          if apresentaumcriterio == "sim":
+          if apresentaumcriterio == "sim" or apresentaumcriterio == "Sim" OR apresentaumcriterio == "s":
               apresentaumcriteriovezes = input("Sibilou menos que 3x ao dia? (Dois episódios no total).\n")
               if apresentaumcriteriovezes == "sim":
                   print("Provavelmente É asma induzida por vírus. Acompanhá-la periodicamente, não necessita iniciar o tratamento preventivo.")
