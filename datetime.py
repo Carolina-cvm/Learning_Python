6521754d209b0f3adfbdf594652d9e17494b8f33
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-17 13:26:56 -0300

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,11 +1,17 @@
-import datetime
+from datetime import datetime, timedelta
+#import datetime
 
-x = datetime.datetime.now()
+data_atual = datetime.now()
 
-print("Hoje é dia:", x.date())
-print("Estamos no ano de:", x.year)
-print("Estamos no mês de:", x.month)
+print("Hoje é dia:", data_atual.date())
+print("Estamos no ano de:", data_atual.year)
+print("Estamos no mês de:", data_atual.month)
 
 # de outra forma
-print(x.strftime("%A,"), x.strftime("%B."))
-print(x.strftime("%x"), "-", x.strftime("%X"))
+print(data_atual.strftime("%A,"), data_atual.strftime("%B."))
+print(data_atual.strftime("%x"), "-", data_atual.strftime("%X"))
+
+# data futura
+data_atual = datetime.now()
+data_futura = data_atual + timedelta(2)
+print("A data daqui dois dias é :", data_futura)
