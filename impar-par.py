f362416ceaf3176433ce63d1ec293bbd2faa8317
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-17 13:26:23 -0300

    Add files via upload

---
+++ impar-par.py
@@ -0,0 +1,9 @@
+def par_ou_impar():
+  if numero % 2 == 0:
+    print(numero, "é número par")
+  else:
+    print(numero, "é número impar")
+
+numero = int(input('Informe um número inteiro qualquer: '))
+par_ou_impar()
+
