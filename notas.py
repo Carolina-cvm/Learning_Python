2c6be911c2c2a7effd71c7038b5a29c541cc85d0
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-20 15:22:48 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -12,3 +12,20 @@ if media >= 7:
     print('APROVADO')
 else:
     print('REPROVADO')
+    
+"""
+nota1 = int(input("Digite a nota:"))
+nota2 = int(input("Digite a nota:"))
+nota3 = int(input("Digite a nota:"))
+nota4 = int(input("Digite a nota:"))
+
+notas = [nota1,nota2,nota3,nota4]
+
+media = sum(notas) / len(notas)
+print("Sua média final foi:", media)
+
+if media >= 7:
+    print("Voce está aprovado")
+else:
+    print("Voce está reprovado")
+"""
