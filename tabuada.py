c3c211e6e9968f254e967124ddf5edff3403c744
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-21 17:36:35 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,11 @@
+def tabuada():
+  print("TABUADA DO",num)
+  contador = 1
+  while contador <= 10:
+    conta = num * contador
+    print(num, "x", contador, "=", conta)
+    contador += 1
+
+num = int(input("Tabuada de qual número? "))
+tabuada()
+    
