abeed7f333b8ccafde0fc5c71d95b1fa1784e2a1
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-10-28 11:50:59 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,31 +1,26 @@
 notas = []
+
+nota1 = int(input("Digite a primeira nota:"))
+nota2 = int(input("Digite a segunda nota:"))
+nota3 = int(input("Digite a terceira nota:"))
+nota4 = int(input("Digite a quarta nota:"))
+
+notas.append(nota1, nota2, nota3, nota4)
+
+media = sum(notas) / len(notas)
+print("Sua média final foi:", media)
+
+if media >= 7:
+  print("Voce está aprovado")
+else:
+  print("Voce está reprovado")
+
+"""
+de outra forma
+notas = []
 contador = 1
 
 while contador < 5:
     notas.append(int(input(f'Informe a {contador}ª nota: ')))
     contador += 1
-
-media = sum(notas) / len(notas)
-print("Sua média final foi:", media)
-
-if media >= 7:
-    print('APROVADO')
-else:
-    print('REPROVADO')
-    
-"""
-nota1 = int(input("Digite a nota:"))
-nota2 = int(input("Digite a nota:"))
-nota3 = int(input("Digite a nota:"))
-nota4 = int(input("Digite a nota:"))
-
-notas = [nota1,nota2,nota3,nota4]
-
-media = sum(notas) / len(notas)
-print("Sua média final foi:", media)
-
-if media >= 7:
-    print("Voce está aprovado")
-else:
-    print("Voce está reprovado")
 """
