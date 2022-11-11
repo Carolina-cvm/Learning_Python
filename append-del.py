3bf6293a8b418b0f8d62a7481c469d14cc434d78
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-11-11 15:47:21 -0300

    Update append-del.py

--- append-del.py
+++ append-del.py
@@ -7,15 +7,15 @@ def excluir_num():
   print(f'Agora esta é a lista de números: {lista}')
 
 lista = [1,2,3,4,5]
-print("Esta é a lista de números:", lista)
+print(f'Esta é a lista de números: {lista}')
 
-alterar = input("Quer adicionar ou excluir um número? ")
+alterar = input(f'Quer adicionar ou excluir um número? ')
 
 if alterar == 'adicionar':
-  adc_num = int(input("Qual numero voce quer adicionar? "))
+  adc_num = int(input(f'Qual numero voce quer adicionar? '))
   adicionar_num()
 elif alterar == 'excluir':
-  exc_num = int(input("Qual a posição do numero que voce quer excluir? "))
+  exc_num = int(input(f'Qual a posição do numero que voce quer excluir? '))
   excluir_num()
 else:
-  print("Algo deu errado, tente novamente mais tarde!")
+  print(f'Algo deu errado, tente novamente mais tarde!')
