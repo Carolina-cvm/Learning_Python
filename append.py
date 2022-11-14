98ba12f73315a6c7f118481409e720386ca572a5
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-11-14 13:40:54 -0300

    Update append.py

--- append.py
+++ append.py
@@ -2,13 +2,13 @@ def adicionar_fruta():
   frutas.append(adc_fruta)
   print(f'Voce adicionou {adc_fruta}, esta é a lista de frutas {frutas}')
 
-frutas = ["morango", "uva","melancia"]
+frutas = ["morango", "uva", "melancia"]
 print(f'Esta é a lista de frutas: {frutas}')
 
 alterar = input("Quer adicionar uma fruta? ")
 
 if alterar == 'adicionar' or alterar == 'sim':
-  adc_fruta = input("Qual fruta voce quer adicionar? ")
+  adc_fruta = input(f'Qual fruta voce quer adicionar? ')
   adicionar_fruta()
 else:
-  print("Algo deu errado, tente novamente mais tarde!")
+  print(f'Algo deu errado, tente novamente mais tarde!')
