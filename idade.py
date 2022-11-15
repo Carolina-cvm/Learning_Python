a290fcb3701b5d8b19ad637c611979f433c03f27
Carolina <55239200+Carolina-cvm@users.noreply.github.com>
2022-11-15 13:53:01 -0300

    Update idade.py

--- idade.py
+++ idade.py
@@ -5,8 +5,8 @@ if 0 <= idade < 12:
 elif 12 <= idade < 18:
   print(f'De acordo com o Estatuto da Criança e do Adolescente, voce é adolescente pois sua idade é {idade}')
 elif 18 <= idade <= 29:
-  print("De acordo com o Estatuto da Juventude, voce é jovem, pois sua idade é", idade)
+  print(f'De acordo com o Estatuto da Juventude, voce é jovem, pois sua idade é {idade}')
 elif 30<= idade <=59:
-  print("Voce é adulto, pois sua idade é", idade)
+  print(f'Voce é adulto, pois sua idade é {idade}')
 else: 
   print("Voce é idoso")
