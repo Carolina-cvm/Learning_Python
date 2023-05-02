1db3095ecc4797d60a6e43ab3a0c7cf4ff06c47e
Carolina Martins <carolina.martins3@pucpr.edu.br>
2023-05-02 12:52:37 -0300

    Create geradordesenhas.py

---
+++ geradordesenhas.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+# Define o conjunto de caracteres permitidos
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+# Define o comprimento da senha
+comprimento = 10
+
+# Gera a senha
+senha = ''.join(random.choice(caracteres) for i in range(comprimento))
+
+# Exibe a senha
+print(senha)
