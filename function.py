5ed9d4d496c110e25ae0a4b365e1c464b5f95f9e
Carolina Martins <carolina.martins3@pucpr.edu.br>
2023-05-17 18:35:38 -0300

    Create function.py

---
+++ function.py
@@ -0,0 +1,21 @@
+def soma(x,y):
+  conta = x + y
+  print(conta)
+
+def subtração(x,y):
+  conta = x - y
+  print(conta)
+  
+def divisão(x,y):
+  conta = x / y
+  print(conta)
+
+def multiplicação(x,y):
+  conta = x * y
+  print(conta)
+
+
+soma(10,5)
+subtração(10,5)
+divisão(10,2)
+multiplicação(10,3)
