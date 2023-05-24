ecbfdc77b8b8f6d44516811376040982b7aff04a
Carolina Martins <carolina.martins3@pucpr.edu.br>
2023-05-24 15:24:17 -0300

    Create math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+import math
+
+x = min(10, 30, 50)
+y = max(100, 150, 200)
+print(x) # 10
+print(y) # 200
+
+z = abs(-7.25)
+print(z) # 7.25
+
+a = pow(4, 3) # 4*4*4
+print(a) # 64
+
+b = math.sqrt(64) # square root
+print(b) # 8.0
+
+c = math.pi
+print(c) # 3.141592653589793
+
